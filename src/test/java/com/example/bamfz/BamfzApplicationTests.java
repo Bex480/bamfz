27e28819f0d6b195b4ad3dfbcbdd0b180fe512bc
Bex <bekir.h480@gmail.com>
2026-04-14 23:45:54 +0200

    init

---
+++ src/test/java/com/example/bamfz/BamfzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bamfz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BamfzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
